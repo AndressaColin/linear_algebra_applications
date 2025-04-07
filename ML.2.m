09104e2a0e9e5fcf4042220c362e708f6bdd42e1
Andressa Colin <104472710+AndressaColin@users.noreply.github.com>
2025-04-07 16:19:42 -0400

    Create ML.2.m

---
+++ ML.2.m
@@ -0,0 +1,14 @@
+% espaco vetorial de polinomios P.n
+
+%polinomios
+p = [2 5 1 -2];
+q = [1 0 3 5];
+
+%p(t) + q(t)
+soma = p + q
+
+%5p(t)
+produto_escalar = 5 * p
+
+%3p(t) - 4q(t)
+combinacao_linear = 3 * p - 4 * q
